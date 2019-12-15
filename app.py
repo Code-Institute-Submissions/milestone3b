24529deab30fc69bbf601e187a63f206473966ba
BrendanMolloy <brendanmolloyonline@gmail.com>
2019-12-15 16:55:07 +0000

    connected to mongoDB

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from env import MONGO_URI
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'suburbanDictionary'
+app.config["MONGO_URI"] = (MONGO_URI)
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_puns')
+def get_puns():
+    return render_template("index.html", puns=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
     app.run(host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", "3000")), debug=True)
+        port=int(os.getenv("PORT", "8080")), debug=True)
---
+++ env.py
@@ -0,0 +1,1 @@
+MONGO_URI = 'mongodb+srv://root:r00tUser@myfirstcluster-avk3w.mongodb.net/suburbanDictionary?retryWrites=true&w=majority'
