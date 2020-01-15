e5a43d2f114724f080345c20a389ce2f5076f1eb
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-01-15 19:19:57 +0000

    defined MONGO_URI on heroku, commented out unnecessary code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from env import MONGO_URI
+#from env import MONGO_URI
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'suburbanDictionary'
