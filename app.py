e63cc633de7ee24b2fde932e4ee4ffc70c8bc3f6
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-01-18 10:05:26 +0000

    spaced out welcome card from main accordion

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
-from env import MONGO_URI
-#if path.exists("env.py"):
-#    from env import MONGO_URI
-#MONGO_URI = os.environ.get('MONGO_URI')
+#from env import MONGO_URI #uncomment this line, and comment out lines 7-9 if using gitpod
+if path.exists("env.py"):
+    from env import MONGO_URI
+MONGO_URI = os.environ.get('MONGO_URI')
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'suburbanDictionary'
@@ -30,17 +30,17 @@ def submit_pun():
     puns.insert_one(request.form.to_dict())
     return redirect(url_for('get_puns'))
 
-@app.route('/profile/<pun_id>')
+@app.route('/profile/<pun_id>') #generates an individual page for each individual pun
 def profile(pun_id):
     the_pun =  mongo.db.puns.find_one({"_id": ObjectId(pun_id)})
     return render_template('profile.html', pun=the_pun)
 
-@app.route('/edit_pun/<pun_id>')
+@app.route('/edit_pun/<pun_id>') #brings user to a form similar to addpun.html, but with the values already filled in for the respective pun to be edited
 def edit_pun(pun_id):
-    the_pun =  mongo.db.puns.find_one({"_id": ObjectId(pun_id)})
+    the_pun =  mongo.db.puns.find_one({"_id": ObjectId(pun_id)}) 
     return render_template('editpun.html', pun=the_pun)
 
-@app.route('/update_pun/<pun_id>', methods=['POST'])
+@app.route('/update_pun/<pun_id>', methods=['POST']) #updates the pun with the data entered at editpun.html
 def update_pun(pun_id):
     puns = mongo.db.puns
     puns.update({'_id': ObjectId(pun_id)},
@@ -52,7 +52,7 @@ def update_pun(pun_id):
     })
     return redirect(url_for('get_puns'))
 
-@app.route('/delete_pun/<pun_id>')
+@app.route('/delete_pun/<pun_id>') #deletes the selected pun
 def delete_pun(pun_id):
     mongo.db.puns.remove({'_id': ObjectId(pun_id)})
     return redirect(url_for('get_puns'))
